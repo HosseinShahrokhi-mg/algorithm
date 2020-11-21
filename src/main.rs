7d2250b258a01ca45871377cc4904bcc8cb99253
hossein <shahrokhi.psm@gmail.com>
2020-11-21 23:50:54 +0330

    change to concerete type : generic to f32

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use std::ops::Sub;
-struct Point<T>{
-    x: T,
-    y: T,
+
+struct Point{
+    x: f32,
+    y: f32,
 }
-impl <T> Point<T>{
-    fn distance_between_points(&self, second_point: Point<T>) -> T{
+impl Point{
+    fn distance_between_points(&self, second_point: Point) -> f32{
         ((self.x - second_point.x).powi(2)+(self.y - second_point.y).powi(2)).sqrt()
+        
     }
 }
 fn main() {
-    let first = Point{x:5, y:10};
-    let second = Point{x:8, y:12};
+    let first = Point{x:5.0, y:10.0};
+    let second = Point{x:8_f32, y:12_f32};
 
     println!("distance = {}",first.distance_between_points(second));
+    //add new code
 }
